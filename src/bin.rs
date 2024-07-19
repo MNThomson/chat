74002750eea572a2d3826b5707a2eaebbc9b0014
Max Thomson <git@mnt.dev>
2024-07-19 15:58:19 -0700

    Split into `Provider` components

--- src/bin.rs
+++ src/bin.rs
@@ -1,56 +1,55 @@
-use std::io::{stdout, Write};
+use std::{
+    env,
+    io::{self, stdout, IsTerminal, Read, Write},
+};
 
 use anyhow::Ok;
-use chat::openai_stream;
-use clap::{Args, Parser, Subcommand};
+use chat::{OpenAI, Provider, ProviderOptions};
+use clap::Parser;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 #[command(propagate_version = true)]
 struct Cli {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    Chat(AddArgs),
-    Image,
-}
-
-#[derive(Args)]
-struct AddArgs {
-    prompt: String,
-    #[arg(short, long, default_value = "gpt-4-turbo")]
+    #[arg(short, long, default_value = "gpt-4o")]
     model: String,
+    prompt: String,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
-    match &cli.command {
-        Commands::Chat(args) => {
-            let mut stream = openai_stream(&args.prompt, &args.model).await?;
+    let stdin_str = if !io::stdin().is_terminal() {
+        let mut stdin_str = String::new();
+        let _ = io::stdin().read_to_string(&mut stdin_str);
+        stdin_str
+    } else {
+        String::from("")
+    };
 
-            let mut lock = stdout().lock();
-            while let Some(result) = stream.recv().await {
-                match result {
-                    Some(response) => {
-                        write!(lock, "{}", response).unwrap();
-                    }
-                    None => {
-                        writeln!(lock).unwrap();
-                        break;
-                    }
-                }
-                stdout().flush()?;
+    let options = ProviderOptions {
+        prompt: format!("{}\n\n{}", cli.prompt, stdin_str)
+            .trim_end()
+            .to_string(),
+        model: cli.model,
+        api_key: env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY not set"),
+    };
+    let mut stream = OpenAI::chat_stream(options).await?;
+
+    let mut lock = stdout().lock();
+    while let Some(result) = stream.recv().await {
+        match result {
+            Some(response) => {
+                write!(lock, "{}", response).unwrap();
             }
-
-            Ok(())
+            None => {
+                writeln!(lock).unwrap();
+                break;
+            }
         }
-        Commands::Image => {
-            todo!("Implement image generation")
-        }
+        stdout().flush()?;
     }
+
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,63 @@
 use async_openai::{
+    config::OpenAIConfig,
     types::{ChatCompletionRequestUserMessageArgs, CreateChatCompletionRequestArgs},
     Client,
 };
 use futures::StreamExt;
 use tokio::sync::mpsc::UnboundedReceiver;
 
-pub async fn openai_stream(
-    prompt: &str,
-    model: &str,
-) -> anyhow::Result<UnboundedReceiver<Option<String>>> {
-    let client = Client::new();
+pub struct ProviderOptions {
+    pub prompt: String,
+    pub model: String,
+    pub api_key: String,
+}
 
-    let request = CreateChatCompletionRequestArgs::default()
-        .model(model)
-        .max_tokens(1024u16)
-        .messages([ChatCompletionRequestUserMessageArgs::default()
-            .content(prompt)
-            .build()?
-            .into()])
-        .build()?;
+pub trait Provider {
+    fn chat_stream(
+        options: ProviderOptions,
+    ) -> impl std::future::Future<Output = anyhow::Result<UnboundedReceiver<Option<String>>>> + Send;
+}
 
-    let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
+pub struct OpenAI {}
+impl Provider for OpenAI {
+    async fn chat_stream(
+        options: ProviderOptions,
+    ) -> anyhow::Result<UnboundedReceiver<Option<String>>> {
+        let client = Client::with_config(OpenAIConfig::new().with_api_key(options.api_key));
 
-    let mut stream = client.chat().create_stream(request).await?;
+        let request = CreateChatCompletionRequestArgs::default()
+            .model(options.model)
+            .max_tokens(1024u16)
+            .messages([ChatCompletionRequestUserMessageArgs::default()
+                .content(options.prompt)
+                .build()?
+                .into()])
+            .build()?;
 
-    tokio::spawn(async move {
-        while let Some(response) = stream.next().await {
-            match response {
-                Ok(response) => {
-                    let text = response.choices[0].delta.content.clone();
-                    if tx.send(text).is_err() {
+        let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
+
+        let mut stream = client.chat().create_stream(request).await?;
+
+        tokio::spawn(async move {
+            while let Some(response) = stream.next().await {
+                match response {
+                    Ok(response) => {
+                        let text = response.choices[0].delta.content.clone();
+                        if tx.send(text).is_err() {
+                            eprintln!("Error in channel");
+                            break;
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("Error in OpenAI stream: {}", e);
                         break;
                     }
                 }
-                Err(e) => {
-                    eprintln!("Error in OpenAI stream: {}", e);
-                    break;
-                }
             }
-        }
 
-        Ok::<(), anyhow::Error>(())
-    });
+            Ok::<(), anyhow::Error>(())
+        });
 
-    Ok(rx)
+        Ok(rx)
+    }
 }
