d9c0889c1903e182fbe9d6594c888320bd5fe2ab
Max Thomson <git@mnt.dev>
2024-07-19 16:28:38 -0700

    Implement `chat.toml` config file

--- src/bin.rs
+++ src/bin.rs
@@ -1,11 +1,13 @@
-use std::{
-    env,
-    io::{self, stdout, IsTerminal, Read, Write},
-};
+use std::io::{self, stdout, IsTerminal, Read, Write};
 
 use anyhow::Ok;
 use chat::{OpenAI, Provider, ProviderOptions};
 use clap::Parser;
+use figment::{
+    providers::{Format, Toml},
+    Figment,
+};
+use serde::Deserialize;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -16,9 +18,19 @@ struct Cli {
     prompt: String,
 }
 
+#[derive(Debug, PartialEq, Deserialize)]
+struct AppConfig {
+    api_key: String,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
+    let dir = dirs::config_dir()
+        .expect("The config dir to exist")
+        .join("chat.toml");
+
+    let figment: AppConfig = Figment::new().merge(Toml::file(dir)).extract().unwrap();
 
     let stdin_str = if !io::stdin().is_terminal() {
         let mut stdin_str = String::new();
@@ -33,7 +45,7 @@ async fn main() -> anyhow::Result<()> {
             .trim_end()
             .to_string(),
         model: cli.model,
-        api_key: env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY not set"),
+        api_key: figment.api_key,
     };
     let mut stream = OpenAI::chat_stream(options).await?;
 
